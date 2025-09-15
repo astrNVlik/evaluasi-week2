dbfc0e3e68be380078153ce619e9015041ecf6fb
astrNVlik <wertybrothers9@gmail.com>
2025-09-15 10:24:38 +0700

    Second commit

---
+++ buku.js
@@ -0,0 +1,25 @@
+
+id="demo"
+const tambahBuku = [superman, harrypotter, bambi]
+
+penerbit.forEach(myfunction)
+
+function myfunction(judul) {
+
+    text += index + ": " + item + "<br>"; 
+}
+
+const judul = {
+
+bambi: "siapa ya lupa",
+harrypotter:"J.K Rowling",
+superman:"Jerry Siegel",
+
+
+
+}
+
+const rilis = [1914, 1990, 1950]
+
+
+
